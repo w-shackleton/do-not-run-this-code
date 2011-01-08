3547463fd603a12845e5d2861d7607cd9850278d
William Shackleton <w.shackleton@gmail.com>
2011-01-08 21:27:44 +0000

    Nothing happened here; just testing code through this. (Need to make test cases)

--- src/pennygame/server/PennyServer.java
+++ src/pennygame/server/PennyServer.java
@@ -1,20 +1,50 @@
 package pennygame.server;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import pennygame.lib.PennyMessage;
 import pennygame.lib.ext.Serialiser;
 
-
-public class PennyServer
-{
+public class PennyServer {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		System.out.println("Hello!");
-		PennyMessage m = new PennyMessage();
-		
-		Serialiser.encode(m, null);
+		try {
+			PennyMessage pm = new PennyMessage();
+			pm.message = "QWERTYUIOP!";
+
+			Socket skt = new Socket("localhost", 1234);
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					skt.getInputStream(), Serialiser.CHARSET));
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+					skt.getOutputStream(), Serialiser.CHARSET));
+
+			Serialiser.encode(pm, out);
+			PennyMessage pm2 = Serialiser.decode(in);
+			System.out.println("Text: " + pm.message);
+			System.out.println("Text2: " + pm2.message);
+
+			in.close();
+			out.close();
+			skt.close();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ConnectException e) {
+			System.out.println("Error: could not connect!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
