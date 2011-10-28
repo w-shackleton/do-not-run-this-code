e7dba931d7d1d7c2444fb34bf35fcd8a2c2194ca
William Shackleton <w.shackleton@gmail.com>
2011-10-29 00:05:08 +0100

    Continued work on Blocks. Added initial attempt at block images

--- spacegame/src/uk/digitalsquid/spacegame/spaceitem/items/Block.java
+++ spacegame/src/uk/digitalsquid/spacegame/spaceitem/items/Block.java
@@ -4,17 +4,65 @@ import javax.microedition.khronos.opengles.GL10;
 
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.Fixture;
+import org.jbox2d.dynamics.FixtureDef;
 
+import uk.digitalsquid.spacegamelib.CompuFuncs;
 import uk.digitalsquid.spacegamelib.SimulationContext;
 import uk.digitalsquid.spacegamelib.spaceitem.SpaceItem;
 import uk.digitalsquid.spacegamelib.spaceitem.interfaces.Forceful;
 import uk.digitalsquid.spacegamelib.spaceitem.interfaces.IsClickable;
 import uk.digitalsquid.spacegamelib.spaceitem.interfaces.Moveable;
 
+/**
+ * Block pieces fit together and are aligned to a grid.
+ * @author william
+ *
+ */
 public class Block extends SpaceItem implements Moveable, Forceful, IsClickable {
+	/**
+	 * The size of this block
+	 */
+	protected Vec2 size;
+	
+	protected Fixture fixture;
 
-	public Block(SimulationContext context, Vec2 pos, float angle, BodyType type) {
-		super(context, pos, angle, type);
+	public Block(SimulationContext context, Vec2 pos, Vec2 size, float angle, BlockDef def) {
+		super(context, pos, angle, BodyType.DYNAMIC);
+		
+		setSize(size, def.getMinSize(), def.getMaxSize());
+		
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = def.getShape();
+		fixtureDef.density = 1;
+		fixtureDef.friction = def.getFriction();
+		fixtureDef.restitution = def.getRestitution();
+		fixture = body.createFixture(fixtureDef);
+		fixture.setUserData(this);
+	}
+	
+	/**
+	 * This contstructor doesn't set up the block's data. This one should probably be overridden.
+	 * @param context
+	 * @param pos
+	 * @param size
+	 * @param angle
+	 */
+	Block(SimulationContext context, Vec2 pos, Vec2 size, float angle) {
+		super(context, pos, angle, BodyType.DYNAMIC);
+		this.size = size;
+	}
+	
+	/**
+	 * Sets the size within an area
+	 * @param size
+	 * @param min
+	 * @param max
+	 */
+	void setSize(Vec2 size, Vec2 min, Vec2 max) {
+		this.size = size;
+		this.size.x = CompuFuncs.TrimMinMax(size.x, min.x, max.x);
+		this.size.y = CompuFuncs.TrimMinMax(size.y, min.y, max.y);
 	}
 
 	@Override
--- spacegame/src/uk/digitalsquid/spacegame/spaceitem/items/BlockDef.java
+++ spacegame/src/uk/digitalsquid/spacegame/spaceitem/items/BlockDef.java
@@ -1,7 +1,12 @@
 package uk.digitalsquid.spacegame.spaceitem.items;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.jbox2d.collision.shapes.Shape;
 import org.jbox2d.common.Vec2;
+
+import uk.digitalsquid.spacegamelib.SimulationContext;
 
 
 /**
@@ -42,4 +47,29 @@ public abstract class BlockDef {
 	 * @return
 	 */
 	public abstract Shape getShape();
+	
+	/**
+	 * Creates a block from this definition. Use this factory method, as returned type may be a child type in some cases.
+	 * The default implementation creates a normal block with this def.
+	 * @return
+	 */
+	public Block create(SimulationContext context, Vec2 pos, Vec2 size, float angle) {
+		return new Block(context, pos, size, angle, this);
+	}
+	
+	private static Map<Integer, BlockDef> defs;
+	
+	/**
+	 * Gets the definition for a block with the given ID. Also initialises the internal data, so should be called from loading screen.
+	 * @param id
+	 * @return <code>null</code> if no def found.
+	 */
+	public static BlockDef getBlockDef(int id) {
+		if(defs == null) {
+			defs = new HashMap<Integer, BlockDef>();
+			
+			// TODO: Define blocks!
+		}
+		return defs.get(id);
+	}
 }
